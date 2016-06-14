711f252ee607974bf1c765af86b5dad6aad7d1c1
Andrew Wonderly <andywonderly@gmail.com>
2016-06-14 17:34:08 -0400

    Changes upon publishing

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,9 +13,14 @@ namespace CarFinder
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
+                            routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "index.html",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
---
+++ obj/Release/Package/PackageTmp/NgApps/Config.js
@@ -0,0 +1,32 @@
+﻿app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('cars', {
+            url: '/',
+            templateUrl: 'NgApps/Templates/Cars.html',
+            controller: 'ctrNewCar'
+        })
+        .state('home', {
+            url: '/home',
+            templateUrl: 'NgApps/Templates/Home.html',
+            controller: 'HomeController'
+        })
+        .state('about', {
+            url: '/aboutpage',
+            templateUrl: 'NgApps/Templates/About.html',
+            controller: 'AboutController'
+        })
+        .state('contact', {
+            url: '/contactpage',
+            templateUrl: 'NgApps/Templates/Contact.html',
+            controller: 'ContactController'
+        })
+        .state('apiHelp', {
+            url: '/apihelp',
+            templateUrl: 'NgApps/Templates/ApiHelp.html',
+            controller: 'ApiHelpController'
+        })
+
+}]);
